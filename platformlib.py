e75926b050175893cea55ccf517f23c11a006309
FilmaBem2 <filmab.emtv2@gmail.com>
2023-01-23 11:08:49 +0000

    Just a simple script to check platform

---
+++ platformlib.py
@@ -0,0 +1,15 @@
+import time
+import platform
+
+if __name__ == '__main__':
+
+    print()
+    # Get platform information
+
+    def os():
+        platform.system()
+    
+    def ver():
+        platform.version()
+
+    

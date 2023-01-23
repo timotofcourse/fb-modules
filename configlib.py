3f2f46541de40c83996024057066196ae426d394
FilmaBem2 <filmab.emtv2@gmail.com>
2023-01-23 11:49:32 +0000

    Config lib added

---
+++ configlib.py
@@ -0,0 +1,40 @@
+import time
+import configparser
+import json
+import io
+
+jsondict = {}
+
+if __name__ == '__main__':
+
+    # JSON save function
+
+    def jsonsave(filename):
+        with open(filename, 'w') as f:
+            json.dump(jsondict, f)
+
+    # JSON load function
+
+    def jsonload(filename):
+        with open(filename, 'r') as f:
+            jsondict.load(f)
+            f.close()
+    
+    # Config parser load function
+
+    def loadconf(filename, section, conf, config):
+        a = configparser.ConfigParser()
+        a.read(filename)
+
+        config = a[section][conf]
+
+    # Config parser save function
+
+    def saveconf(filename, section, conf, config):
+        a = configparser.ConfigParser()
+        a.read(filename)
+        a[section][conf]=config
+        with open(filename, "w") as file_object:
+            a.write(file_object)
+
+    

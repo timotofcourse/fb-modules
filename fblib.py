0c2c9ae85c7bf04a7f440eada48c1b7c98e4d9a2
FilmaBem2 <filmab.emtv2@gmail.com>
2023-01-23 15:35:17 +0000

    New functions

--- fblib.py
+++ fblib.py
@@ -1,10 +1,15 @@
 import time
 import os
 from PIL import Image
+from multiprocess import process, Manager
+
+pros = []
+textures = 'assets/textures/'
+audio = 'assets/audio/'
+images = 'assets/imgs/'
 
 
 if __name__ == '__main__':
-    time.sleep()
 
     # Check if a file exists
 
@@ -16,5 +21,21 @@ if __name__ == '__main__':
     def img(load):
         Image.open(load)
 
+    def proclist(proc):
+        pros.append(proc)
+    
+    # Multiprocess function
+
+    def multi(var, procs):
+        with Manager():
+            pros = Manager.list()
+            processes = []
+            for i in range(5):
+                var = process(target=proclist, args=(procs, i,)
+                var.start()
+                processes.append(p)
+            for p in processes:
+                p.join()
+        
     
         

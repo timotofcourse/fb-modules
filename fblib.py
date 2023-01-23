a336666452688540157f2ffc743be537f5fad50e
FilmaBem2 <filmab.emtv2@gmail.com>
2023-01-23 13:54:34 +0000

    Added generic lib

---
+++ fblib.py
@@ -0,0 +1,14 @@
+import time
+import os
+
+
+
+if __name__ == '__main__':
+    time.sleep()
+
+    # Check if a file exists
+
+    def find(filename, var):
+        var = os.path.exists(filename)
+
+    

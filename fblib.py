0311ae775399f637108835cc1da349c27bf23958
Filma Bem <filmab.emtv2@gmail.com>
2023-02-07 17:42:08 +0000

    Added audio function

--- fblib.py
+++ fblib.py
@@ -14,6 +14,10 @@ audio = 'assets/audio/' # Location for audios
 images = 'assets/imgs/' # Location for other images that are not textures
 models = 'assets/models' # Location for models if i need them
 
+# Initiate the audio mixer
+
+mixer.init()
+
 # Check if a file exists
 
 def find(filename, var):
@@ -27,3 +31,11 @@ def img(load):
 def proclist(proc):
     pros.append(proc)
     
+# Audio function for games and other apps that i need to play some audio. I require to pass the mtime because i want to control the time manually instead of getting it from the mp3 files
+
+def gaudio(audiofile, audiovolume, mtime):
+    mixer.music.load(audiofile)
+    mixer.music.set_volume(audiovolume)
+    mixer.music.play()
+    time.sleep(mtime)
+    mixer.music.stop()

9ed949b2b1b1485b1c4a0daa1124c648db2b2322
Filma Bem <filmab.emtv2@gmail.com>
2023-02-07 17:54:06 +0000

    Removed unneded arguments for the first function

--- pacmanlib.py
+++ pacmanlib.py
@@ -2,22 +2,17 @@ import pacman
 import subprocess
 import time
 
-def pacmanager(pm, var, inst, upd, confirm, pkgs, var2, remove, action):
+def pacmanager(pm, inst, upd, confirm, pkgs, remove, action):
     if pm == 'yay' or 'paru':
         if action == 'install':
-            var = subprocess.Popen(pm + ' ' + inst + ' ' + confirm)
-            var.wait()
+            subprocess.Popen(pm + ' ' + inst + ' ' + confirm).wait()
         elif action == 'update':
-            var = subprocess.Popen(pm + ' ' + upd + ' ' + confirm)
-            var.wait()
+            subprocess.Popen(pm + ' ' + upd + ' ' + confirm).wait()
         elif action == 'remove':
-            var = subprocess.Popen(pm + ' ' + remove + ' ' + confirm)
-            var.wait()
+            subprocess.Popen(pm + ' ' + remove + ' ' + confirm).wait()
         elif action == 'update and install':
-            var2 = subprocess.Popen(pm + ' ' + upd + ' ' + confirm)
-            var2.wait()
-            var = subprocess.Popen(pm + ' ' + inst + ' ' + confirm)
-            var.wait()
+            subprocess.Popen(pm + ' ' + upd + ' ' + confirm).wait()
+            subprocess.Popen(pm + ' ' + inst + ' ' + confirm).wait()
         else:
             print('Action not supported')
     elif pm == pacman:
@@ -29,27 +24,20 @@ def pacmanager(pm, var, inst, upd, confirm, pkgs, var2, remove, action):
             pacman.remove(pkgs)
             pacman.needs_for(pkgs)
         elif action == 'update and install':
-            var2 = subprocess.Popen(pm + ' ' + upd + ' ' + confirm)
-            var2.wait()
-            var = subprocess.Popen(pm + ' ' + inst + ' ' + confirm)
-            var.wait()
+            subprocess.Popen(pm + ' ' + upd + ' ' + confirm).wait()
+            subprocess.Popen(pm + ' ' + inst + ' ' + confirm).wait()
         else:
             print('Action not supported')
     else :
         if action == 'install':
-            var = subprocess.Popen('sudo ' + pm + ' ' + inst + ' ' + pkgs + ' ' + confirm)
-            var.wait()
+            subprocess.Popen('sudo ' + pm + ' ' + inst + ' ' + pkgs + ' ' + confirm).wait()
         elif action == 'update':
-            var2 = subprocess.Popen('sudo ' + pm + ' ' + upd + ' ' + confirm)
-            var2.wait()
+            subprocess.Popen('sudo ' + pm + ' ' + upd + ' ' + confirm).wait()
         elif action == 'remove':
-            var = subprocess.Popen('sudo ' + pm + ' ' + remove + ' ' + confirm)
-            var.wait()
+            subprocess.Popen('sudo ' + pm + ' ' + remove + ' ' + confirm).wait()
         elif action == 'update and install':
-            var2 = subprocess.Popen('sudo ' + pm + ' ' + upd + ' ' + confirm)
-            var2.wait()
-            var = subprocess.Popen('sudo ' + pm + ' ' + inst + ' ' + pkgs + ' ' + confirm)
-            var.wait()
+            subprocess.Popen('sudo ' + pm + ' ' + upd + ' ' + confirm).wait()
+            subprocess.Popen('sudo ' + pm + ' ' + inst + ' ' + pkgs + ' ' + confirm).wait()
         else:
             print('Action not supported')
             

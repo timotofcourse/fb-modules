90951e9b48e1c5172a5a603c8f89337e1733446d
Filma Bem <filmab.emtv2@gmail.com>
2023-02-07 17:46:20 +0000

    Fixed Identation

--- configlib.py
+++ configlib.py
@@ -3,35 +3,33 @@ import json
 
 jsondict = {}
 
-if __name__ == '__main__':
+# JSON save function
 
-    # JSON save function
+def jsonsave(filename):
+    with open(filename, 'w') as f:
+        json.dump(jsondict, f)
 
-    def jsonsave(filename):
-        with open(filename, 'w') as f:
-            json.dump(jsondict, f)
+# JSON load function
 
-    # JSON load function
-
-    def jsonload(filename):
-        with open(filename, 'r') as f:
-            jsondict.load(f)
-            f.close()
+def jsonload(filename):
+    with open(filename, 'r') as f:
+        jsondict.load(f)
+        f.close()
     
-    # Config parser load function
+# Config parser load function
 
-    def loadconf(filename, section, conf, config):
-        a = configparser.ConfigParser()
-        a.read(filename)
+def loadconf(filename, section, conf, config):
+    a = configparser.ConfigParser()
+    a.read(filename)
 
-        config = a[section][conf]
+    config = a[section][conf]
 
-    # Config parser save function
+# Config parser save function
 
-    def saveconf(filename, section, conf, config):
-        a = configparser.ConfigParser()
-        a.read(filename)
-        a[section][conf]=config
-        with open(filename, "w") as file_object:
-            a.write(file_object)
+def saveconf(filename, section, conf, config):
+    a = configparser.ConfigParser()
+    a.read(filename)
+    a[section][conf]=config
+    with open(filename, "w") as file_object:
+        a.write(file_object)
 

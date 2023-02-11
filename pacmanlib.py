e79e3f0d13a5b309944b74b9d85184156e17e06d
Filma Bem <filmab.emtv2@gmail.com>
2023-02-11 19:16:08 +0000

    Replaced old code with better code assisted by AI

--- pacmanlib.py
+++ pacmanlib.py
@@ -1,68 +1,91 @@
-import pacman
+
+import platform
 import subprocess
 import os
 
-# Home 
-home = os.path.expanduser( '~' )
-
-def pacmanager(pm, inst, upd, confirm, pkgs, remove, action):
-    if pm == 'yay' or 'paru': # Yay or Paru AUR Helpers. I Could add more but i'm lazy and i can add them later anyway
-        if action == 'install':
-            subprocess.Popen(pm + ' ' + inst + ' ' + confirm).wait()
-        elif action == 'update':
-            subprocess.Popen(pm + ' ' + upd + ' ' + confirm).wait()
-        elif action == 'remove':
-            subprocess.Popen(pm + ' ' + remove + ' ' + confirm).wait()
-        elif action == 'update and install':
-            subprocess.Popen(pm + ' ' + upd + ' ' + confirm).wait()
-            subprocess.Popen(pm + ' ' + inst + ' ' + confirm).wait()
+def install_package(package_name):
+    system = platform.system()
+    if system == 'Linux':
+        dist = platform.linux_distribution()[0]
+        if dist in ['Ubuntu', 'Debian']:
+            subprocess.run(['sudo', 'apt', 'install', '-y', package_name])
+        elif dist in ['Fedora']:
+            subprocess.run(['sudo', 'dnf', 'install', '-y', package_name])
+        elif dist in ['arch']:
+            subprocess.run(['sudo', 'pacman', '-S', '--noconfirm', package_name])
         else:
-            print('Action not supported') # Code for Debug purposes
-    elif pm == pacman: # If Pacman use the python-pacman module to do this actions
-        if action == 'install':
-            pacman.install(pkgs)
-        elif action == 'update':
-            pacman.upgrade()
-        elif action == 'remove':
-            pacman.remove(pkgs)
-            pacman.needs_for(pkgs)
-        elif action == 'update and install':
-            subprocess.Popen(pm + ' ' + upd + ' ' + confirm).wait()
-            subprocess.Popen(pm + ' ' + inst + ' ' + confirm).wait()
+            print(f"Package manager not supported for {dist}")
+    elif system == 'Windows':
+        if os.path.exists("C:\\Program Files\\WindowsApps\\Winget\\Winget.exe"):
+            subprocess.run(['winget', 'install', '-e', package_name])
+        elif os.path.exists("C:\\ProgramData\\chocolatey\\bin\\choco.exe"):
+            subprocess.run(['cmd.exe', '/c', 'start', 'cmd.exe', '/k', 'choco', 'install', package_name, '-y'], shell=True)
         else:
-            print('Action not supported') # Code for Debug purposes
-    else :
-        if action == 'install':
-            subprocess.Popen('sudo ' + pm + ' ' + inst + ' ' + pkgs + ' ' + confirm).wait()
-        elif action == 'update':
-            subprocess.Popen('sudo ' + pm + ' ' + upd + ' ' + confirm).wait()
-        elif action == 'remove':
-            subprocess.Popen('sudo ' + pm + ' ' + remove + ' ' + confirm).wait()
-        elif action == 'update and install':
-            subprocess.Popen('sudo ' + pm + ' ' + upd + ' ' + confirm).wait()
-            subprocess.Popen('sudo ' + pm + ' ' + inst + ' ' + pkgs + ' ' + confirm).wait()
+            print("Package manager not supported")
+    else:
+        print(f"Package manager not supported for {system}")
+
+def remove_package(package_name):
+    system = platform.system()
+    if system == 'Linux':
+        dist = platform.linux_distribution()[0]
+        if dist in ['Ubuntu', 'Debian']:
+            subprocess.run(['sudo', 'apt', 'remove', '-y', package_name])
+        elif dist in ['Fedora']:
+            subprocess.run(['sudo', 'dnf', 'remove', '-y', package_name])
+        elif dist in ['arch']:
+            subprocess.run(['sudo', 'pacman', '-R', '--noconfirm', package_name])
         else:
-            print('Action not supported') # Code for Debug purposes
-          
-# Windows Package Managers Path
+            print(f"Package manager not supported for {dist}")
+    elif system == 'Windows':
+        if os.path.exists("C:\\Program Files\\WindowsApps\\Winget\\Winget.exe"):
+            subprocess.run(['winget', 'remove', '-e', package_name])
+        elif os.path.exists("C:\\ProgramData\\chocolatey\\bin\\choco.exe"):
+            subprocess.run(['cmd.exe', '/c', 'start', 'cmd.exe', '/k', 'choco', 'uninstall', package_name, '-y'], shell=True)
+        elif os.path.exists("C:\\ProgramData\\Scoop\\shim\\scoop.exe"):
+            subprocess.run(['scoop', 'uninstall', package_name])
+        else:
+            print("Package manager not supported")
+    else:
+        print(f"Package manager not supported for {system}")
 
-scoop_path = '/scoop'
-choco_path = 'C:/ProgramData/chocolatey/bin'
-            
-def checkwindowspm(): # Check if windows package managers are installed
-    os.path.exists()
+def update_system():
+    system = platform.system()
+    if system == 'Linux':
+        dist = platform.linux_distribution()[0]
+        if dist in ['Ubuntu', 'Debian']:
+            subprocess.run(['sudo', 'apt', 'update'])
+            subprocess.run(['sudo', 'apt', 'upgrade', '-y'])
+        else:
+          if dist in ['Fedora']:
+              subprocess.run(['sudo', 'dnf', 'update', '-y'])
+          elif dist in ['arch']:
+              subprocess.run(['sudo', 'pacman', '-Syu', '--noconfirm'])
+          else:
+              print(f"Package manager not supported for {dist}")
+    elif system == 'Windows':
+        if os.path.exists("C:\Program Files\WindowsApps\Winget\Winget.exe"):
+            subprocess.run(['winget', 'update'])
+        elif os.path.exists("C:\ProgramData\chocolatey\bin\choco.exe"):
+            subprocess.run(['cmd.exe', '/c', 'start', 'cmd.exe', '/k', 'choco', 'upgrade', 'all', '-y'], shell=True)
+        elif os.path.exists("C:\\ProgramData\\Scoop\\shim\\scoop.exe"):
+            subprocess.run(['scoop', 'update', 'all'])
+        else:
+            print("Package manager not supported")
+    else:
+        print(f"Package manager not supported for {system}")
 
-if checkwindowspm(scoop_path):
-    print('Scoop Installed')
-else:
-    subprocess.Popen('irm get.scoop.sh | iex', shell=True).wait()
-
-if checkwindowspm(choco_path):
-    print('Chocolatey Installed')
-else:
-    subprocess.Popen("Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))", shell=True).wait()
-
-            
-def winpm(pm, action, pkgs, args):
-    subprocess.Popen(pm + ' ' + action + '' + pkgs + '' + args, shell=True).wait()
-    
+def remove_unneeded_dependencies():
+  system = platform.system()
+  if system == 'Linux':
+    dist = platform.linux_distribution()[0]
+    if dist in ['Ubuntu', 'Debian']:
+        subprocess.run(['sudo', 'apt', 'autoremove', '-y'])
+    elif dist in ['Fedora']:
+        subprocess.run(['sudo', 'dnf', 'autoremove', '-y'])
+    elif dist in ['arch']:
+        subprocess.run(['sudo', 'pacman', '-Rns', '--noconfirm', '$(pacman -Qtdq)'])
+    else:
+        print(f"Package manager not supported for {dist}")
+  else:
+    print(f"Package manager not supported for {system}")

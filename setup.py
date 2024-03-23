6b80f5d59149af372a266dca2269149d3bd18d79
Timot <filmab.emtv2@gmail.com>
2024-03-23 14:12:06 +0000

    Added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -7,17 +7,24 @@ with open('LICENSE.MD', 'r') as f:
     license = f.read()
     
 setup(
-    name='fb-modules',
+    name='fbmodules',
     version='0.0.1',
     description='Just a simple set of modules ',
     long_description=readme,
+    long_description_content_type='text/markdown',
     package_dir={'': 'fbmodules'},
-    packages=find_packages(where='fb-modules'),
+    packages=find_packages(where='fbmodules'),
     author='Timot',
     author_email='',
     url='https://github.com/timotofcourse/fb-modules',
     license=license,
+    classiifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+        'Operating System :: OS Independent',
+        ],
     packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=['ruamel.yaml', 'shutil', 'ctypes', 'winotify'],
     extras_require={
         'dev': ['twine'],
     },

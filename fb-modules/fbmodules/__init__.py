76ea58fb7aae1d1db00d9e1f8223759367673194
Timot <filmab.emtv2@gmail.com>
2024-03-23 13:45:32 +0000

    Added a few more functions

--- fb-modules/fbmodules/__init__.py
+++ fb-modules/fbmodules/__init__.py
@@ -1,5 +1,18 @@
 from .src.fbmodules import (
-    enable_windows_feature
+    enable_windows_feature,
+    disable_windows_feature,
+    enable_windows_hibernation,
+    disable__windows_hibernation,
+    load_yaml,
+    run_command,
+    change_owner,
+    get_current_user,
+    check_for_admin,
+    get_admin,
+    help_dialog,
+    is_offline,
+    windows_messagebox,
+    windows_notifications
 )
 from .src.web import(
     open_link,
--- fb-modules/fbmodules/src/fbmodules.py
+++ fb-modules/fbmodules/src/fbmodules.py
@@ -1,15 +1,18 @@
 from os import system, getlogin
 from ruamel.yaml import YAML
 import shutil
+from ctypes import windll
+import sys
+from winotify import Notification, audio
 
 # Enable Windows Feature
 
-def enable_windows_feature(feature_name):
+def enable_windows_feature(feature_name: str):
     system(f'dism /Online /Enable-Feature /{feature_name}')
     
 # Disable Windows Feature
 
-def enable_windows_feature(feature_name):
+def enable_windows_feature(feature_name: str):
     system(f'dism /Online /Disable-Feature /{feature_name}')
 
 # Enable Windows Hibernation
@@ -24,7 +27,7 @@ def disable__windows_hibernation():
     
 # Load Yaml
 
-def load_yaml(file):
+def load_yaml(file: str):
     yaml = YAML(typ='safe')
     with open(file, 'r', encoding='utf-8') as yaml_file:
         yaml_data = yaml.load(yaml_file)
@@ -32,12 +35,12 @@ def load_yaml(file):
 
 # Run command
 
-def run_command(command):
+def run_command(command: str):
     system(command)
     
 # Change owner
 
-def change_owner(path, user=None, group=None):
+def change_owner(path: str, user: str=None, group: str=None):
     shutil.chown(path, user, group)
     
 # get current user
@@ -45,3 +48,41 @@ def change_owner(path, user=None, group=None):
 def get_current_user():
     getlogin()
     
+# Check for admin
+
+def check_for_admin():
+    try:
+        return windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+    
+# Get admin
+
+def get_admin():
+    return windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+    
+# Help dialog
+
+def help_dialog(title: str, text: str, icon: str):
+    windll.shell32.ShellAboutA(None, title, text, icon)
+
+# Check if internet is offline
+
+def is_offline():
+    return windll.shell32.InetIsOffline(None)
+
+# Windows messagebox
+
+def windows_messagebox(title: str, message: str, type: str):
+    # Logic for the type of messagebox
+    ws_ex = ''
+    return windll.user32.MessageBoxExW(None, message, title, ws_ex)
+
+# Windows notifications
+
+def windows_notifications(app_id:str, title: str, msg: str, icon: str=None):
+    notification = Notification(app_id, title, msg, icon)
+    notification.set_audio(audio.Default, loop=False)
+    notification.show()
+
+
